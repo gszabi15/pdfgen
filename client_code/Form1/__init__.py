eec0d690e7c8bfe0907e810ef112308184f28eec
gombos.szabolcs15@gmail.com <gombos.szabolcs15@gmail.com>
2024-11-06 11:29:28 +0000

    Created ServerModule1; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,10 +1,13 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
 
 
 class Form1(Form1Template):
+  def click(self, **properties):
+    self.outlined_button_1.visible = 0
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
+    self.outlined_button_1.add_event_handler('click',self.click)
     # Any code you write here will run before the form opens.
