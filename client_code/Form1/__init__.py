ee98bb92d21a8f7ad505adeb7cd705c042bb4dbb
gombos.szabolcs15@gmail.com <gombos.szabolcs15@gmail.com>
2024-11-06 19:49:12 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -7,7 +7,7 @@ class Form1(Form1Template):
   #def click(self, **properties):
   #  self.outlined_button_1.visible = 0
   def __init__(self, **properties):
-    self.column_panel_1.width = "660px"
+    self.column_panel_1.width = "700px"
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     #self.outlined_button_1.add_event_handler('click',self.click)
