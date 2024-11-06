cd7a9851827208105219b2d360e213172a337dbb
gombos.szabolcs15@gmail.com <gombos.szabolcs15@gmail.com>
2024-11-06 14:51:55 +0000

    Created Form1.ItemTemplate1, Form1.ItemTemplate2, Form1.ItemTemplate3, Form1.RowTemplate1 and 1 more; Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -4,10 +4,11 @@ import anvil.server
 
 
 class Form1(Form1Template):
-  def click(self, **properties):
-    self.outlined_button_1.visible = 0
+  #def click(self, **properties):
+  #  self.outlined_button_1.visible = 0
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.outlined_button_1.add_event_handler('click',self.click)
+    #self.outlined_button_1.add_event_handler('click',self.click)
     # Any code you write here will run before the form opens.
+
