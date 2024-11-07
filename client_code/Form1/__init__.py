fb1393c88ad1d4dc32d217c2f0c9f807e0b35f04
gombos.szabolcs15@gmail.com <gombos.szabolcs15@gmail.com>
2024-11-07 22:19:16 +0000

    Edited Form1 and Form1.RowTemplate4

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -7,10 +7,11 @@ class Form1(Form1Template):
   #def click(self, **properties):
   #  self.outlined_button_1.visible = 0
   def __init__(self, **properties):
-    self.column_panel_1.width = "720px"
+    self.column_panel_1.width = "1110px"
+    self.column_panel_4.width = "500px"
     #self.varazstargyak.items = [{'varazstargyak': '', 'hatas': '','mp':'','db':''}]
     self.init_components(**properties)
     #self.outlined_button_1.add_event_handler('click',self.click)
     # Any code you write here will run before the form opens.
-    for col in self.repeating_panel_2.columns:
-      self.repeating_panel_2.add_component(TextBox(), column=col["id"])
+    self.repeating_panel_2.items = {'varazstargyak_1':'','varazstargyak_2':'','varazstargyak_3':'','varazstargyak_4':'','varazstargyak_5':''}
+    self.repeating_panel.items = {'ke_1':'','te_1':'','ve_1':'','ce_1':''}
