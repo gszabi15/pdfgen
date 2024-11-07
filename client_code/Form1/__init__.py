c2b25cdf1e8d634250b9997fd57069aefac946b5
gombos.szabolcs15@gmail.com <gombos.szabolcs15@gmail.com>
2024-11-07 14:07:45 +0000

    Created Form1.RowTemplate3, Form1.ItemTemplate4 and Form1.ItemTemplate5; Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -8,7 +8,7 @@ class Form1(Form1Template):
   #  self.outlined_button_1.visible = 0
   def __init__(self, **properties):
     self.column_panel_1.width = "720px"
-    # Set Form properties and Data Bindings.
+    #self.varazstargyak.items = [{'varazstargyak': '', 'hatas': '','mp':'','db':''}]
     self.init_components(**properties)
     #self.outlined_button_1.add_event_handler('click',self.click)
     # Any code you write here will run before the form opens.
