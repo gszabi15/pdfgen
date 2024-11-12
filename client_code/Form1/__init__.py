d221b96c049db53bb5ad763b263c1461e69d0510
gombos.szabolcs15@gmail.com <gombos.szabolcs15@gmail.com>
2024-11-12 14:40:52 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -183,34 +183,53 @@ class Form1(Form1Template):
       delete.enabled = False
     if len(rep.items) < 7:
       add.enabled = True
-
+  def getfegyver(self):
+    out = []
+    for i in range(5):
+      for row in self.fegyver_1.get_components() if i < 3 else self.fegyver_2.get_components() if i == 3 else self.fegyver_2.get_components() if i == 4 else []:
+        fegyver = []
+        fegyver += [row.nev.text]
+        fegyver += [row.ke.text]
+        fegyver += [row.te.text]
+        fegyver += [row.ve.text]
+        fegyver += [row.tam.text]
+        fegyver += [row.sebzes.text]
+        fegyver += [row.pajzs.text]
+        fegyver += [row.magikus.checked]
+        fegyver += [row.aldott.checked]
+        out += [fegyver]
+      
+    return out
   def mentes_click(self, **event_args):
+    elony = [item for sublist in self.getrow(self.elony) for item in sublist]
+    hatrany = [item for sublist in self.getrow(self.hatrany) for item in sublist]
+    print(self.getfegyver())
     data = {
     "": {
         "alap": [self.nev.text, self.kor_box.text, self.kaszt_box.text, self.faj_box.text, self.vallas_box.text, self.szarmazas_box.text, self.csillagjegy_box.text, self.rend_box.text, self.rang_box.text, self.csapat_box.text, self.falka_box.text, self.jelmondat_box.text, self.jelmondat2_box.text,self.szint.text,self.jellem_1.text,self.jellem_2.text],
         "stat": [self.ero.text, self.gyorsasag.text, self.ugyesseg.text, self.allokepesseg.text, self.egeszseg.text, self.szepseg.text, self.intelligencia.text, self.akaratero.text, self.asztral.text, self.eszleles.text],
         "szazalek": [self.titkosajto_kereses.text, self.ugras.text, self.eses.text, self.maszas.text, self.zsebmetszes.text, self.zarnyitas.text, self.rejtozes.text, self.koteltanc.text, self.lopozas.text, self.csapdafelfedezes.text],
-        "elony": ["Erős lélek (Ego: 83)", "", "", "", ""],
-        "hatrany": ["", "", "", "", ""], 
+        "elony": elony + ['']*(5-len(elony)),
+        "hatrany": hatrany + ['']*(5-len(hatrany)), 
         "varazstargyak": [["", "", "", ""], ["", "", "", ""], ["", "", "", ""], ["", "", "", ""]],
         "fegyver_nelkuli": [self.ke.text, self.te.text, self.ve.text, self.ce.text],
         "fegyver": [["Tőrlard", "25", "50", "127", "2", "1K6+2", "", False, False], ["", "", "", "", "", "", "", False, False], ["", "", "", "", "", "", "", False, False]],
-        "pszi": [self.pszi.text, self.termeszetes_asztral, self.statikus_asztral, self.dinamikus_asztral, self.me_asztral, self.termeszetes_mental, self.statikus_mental, self.dinamikus_mental, self.me_mental, self.pszi_af.checked, self.pszi_mf.checked, self.pszi_pyarroni.checked, self.pszi_slan.checked, self.pszi_kyr.checked, self.pszi_siopa.checked],
+        "pszi": [self.pszi.text, self.termeszetes_asztral.text, self.statikus_asztral.text, self.dinamikus_asztral.text, self.me_asztral.text, self.termeszetes_mental.text, self.statikus_mental.text, self.dinamikus_mental.text, self.me_mental.text, self.pszi_af.checked, self.pszi_mf.checked, self.pszi_pyarroni.checked, self.pszi_slan.checked, self.pszi_kyr.checked, self.pszi_siopa.checked],
         "varazsero": [self.mana.text, self.od.text, self.verpont.text, self.elet.checked, self.lelek.checked, self.termeszet.checked, self.halal.checked],
         "vert": [self.vert.text, self.ep.text, self.fp.text, self.sfe.text, self.mgt.text, self.nehezvert_af.checked, self.nehezvert_mf.checked],
         "levonasok": [["","","",""],["","","",""]],
-        "penz": [[self.arany_1.text, self.ezust_1.text, self.rez_1.text, self.hol_1.text],[self.arany_2.text, self.ezust_2.text, self.rez_2.text, self.hol_2],[self.arany_3.text, self.ezust_3.text, self.rez_3.text, self.hol_3.text]],
+        "penz": [[self.arany_1.text, self.ezust_1.text, self.rez_1.text, self.hol_1.text],[self.arany_2.text, self.ezust_2.text, self.rez_2.text, self.hol_2.text],[self.arany_3.text, self.ezust_3.text, self.rez_3.text, self.hol_3.text]],
         "nyelv": [[["Gorviki","Erv","Pyar","Dzsad","Toroni"],["5","5","5","4","3"]],[["Aszisz","Shadoni","","",""],["3","3","","",""]]],
-        "felszereles": [[["Varázslóbot",""],["ing, nemesi",""],["kesztyű, nemesi",""],["Kaftán",""],["prém köpeny",""],["Alapellátáshoz szükséges felszerelés",""],["Utazó, egyszerű alkimista felszerelés",""],["SKCS",""]],[["",""]]],
-        "kepzettseg": [[["fegyverhasználat (Tőrkard)", False, True],["rúnamágia", False, True],["ásványtan", False, True],["alkímia", False, True],["etikett", False, True],["ősi nyelv ismerete (Kyr)", True, False],["írás/olvasás", True, False],["legendaismeret", True, False],["történelemismeret", True, False],["sebgyógyítás", True, False],["heraldika", True, False],["építészet", True, False],["térképészet", True, False],["vallásismeret", True, False],["szexuális kultúra", True, False],["tánc", True, False],["úszás", True, False],["lovaglás", True, False],["kocsihajtás", True, False],["értékbecslés", True, False],["hamiskártya", True, False]],[["", True, False]]],
-        "mergek": [["","","",""],["","","",""],["","","",""],["","","",""]],
+        "felszereles": [self.getrow(self.lathato_felszereles),self.getrow(self.nem_lathato_felszereles)],
+        "kepzettseg": [self.getrow(self.kepzettsegek_1),self.getrow(self.kepzettsegek_2)],
+        "mergek": self.getrow(self.mergek),
         "tarsak": [["Ló, utazó", "", "", "", "", "", "", "", "", "", ""], ["", "", "", "", "", "", "", "", "", "", ""]],
-        "talalkozok": [["","","","","",""],["","","","","",""],["","","","","",""],["","","","","",""],["","","","","",""],["","","","","",""]],
-        "tp": "", 
-        "zsakmany": [["",""],["",""],["",""],["",""],["",""],["",""],["",""]]
+        "talalkozok": self.getrow(self.rendezvenyek),
+        "tp": self.tp.text, 
+        "zsakmany": self.getrow(self.zsakmany)
     }
 }
-    anvil.media.download(anvil.server.call('retgenpdf',data,self.nev.text.replace(" ","_")+"_karakterlap"))
+    #anvil.media.download(anvil.server.call('retgenpdf',data,self.nev.text.replace(" ","_")+"_karakterlap"))
 
   def ero_pressed_enter(self, **event_args):
     """This method is called when the user presses Enter in this text box"""
