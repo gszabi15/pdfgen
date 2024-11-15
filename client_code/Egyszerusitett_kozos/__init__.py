5a2c420554ba90fc958398728fec3e33de9dc05d
gombos.szabolcs15@gmail.com <gombos.szabolcs15@gmail.com>
2024-11-15 00:52:53 +0000

    Edited Egyszerusitett_kozos and PDFgen

--- client_code/Egyszerusitett_kozos/__init__.py
+++ client_code/Egyszerusitett_kozos/__init__.py
@@ -15,8 +15,15 @@ class Egyszerusitett_kozos(Egyszerusitett_kozosTemplate):
   def panel1_visable(self):
     if self.faj.selected_value != "Faj" and self.kaszt.selected_value != "Kaszt":
       self.column_panel_3.visible = True
+    else:
+      self.column_panel_3.visible = False
   def faj_change(self, **event_args):
     self.panel1_visable()
+    if self.faj.selected_value != "Faj":
+      self.kaszt.enabled = True
+    else:
+      self.kaszt.enabled = False
+      self.kaszt.selected_value = "Kaszt"
 
   def kaszt_change(self, **event_args):
    self.panel1_visable()
